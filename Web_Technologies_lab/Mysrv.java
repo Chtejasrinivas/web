8465d22bea438c1f5d76de73e36563e045821334
Chtejasrinivas <80935060+Chtejasrinivas@users.noreply.github.com>
2021-11-12 14:30:00 +0530

    Add files via upload

---
+++ Web_Technologies_lab/Mysrv.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import javax.servlet.*;
+public class Mysrv extends GenericServlet
+{
+public void service(ServletRequest req,ServletResponse res) throws ServletException,IOException
+{
+PrintWriter out=res.getWriter()
+res.setContentType("text/html");
+out.println("<h1>Welcome to Servlets ☺ </h1>");
+out.close();
+}
+}
